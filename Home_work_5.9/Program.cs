cf5cbbe2de14e71e670f1624f911a8e31f529dd0
Maxim Konev <sayberix.ru@yandex.ru>
2022-07-28 21:36:44 +0700

    условия для задачи 5.9.2

--- Home_work_5.9/Program.cs
+++ Home_work_5.9/Program.cs
@@ -6,20 +6,11 @@ namespace Home_work_5._9
     {
         static void Main(string[] args)
         {
-            // Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
-            // M = 1; N = 5. -> ""1, 2, 3, 4, 5""
-            // M = 4; N = 8. -> ""4, 6, 7, 8""
+            // Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+            // M = 1; N = 15-> 120
+            // M = 4; N = 8. -> 30
 
-            int m = 5; //Convert.ToInt32(Console.ReadLine());
-            int n = 25; //Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine(Func(m, n));
-
-            string Func(int start, int end)
-            {
-                if (start == end) return start.ToString();
-                return (start + "," + Func(start + 1, end));
-            }
         }
     }
 }
