aff5def2568212ed3cc8565d0a79cf94cd0cf2e0
Maxim Konev <sayberix.ru@yandex.ru>
2022-07-28 23:43:48 +0700

    задача 5.9.3 выполнена

--- Home_work_5.9/Program.cs
+++ Home_work_5.9/Program.cs
@@ -2,14 +2,31 @@
 
 namespace Home_work_5._9
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
             // m = 2, n = 3 -> A(m, n) = 29
 
+            int m = 0; //Convert.ToInt32(Console.ReadLine());
+            int n = 5; //Convert.ToInt32(Console.ReadLine());
+            //int calcAnkerman = m, coefAnkerman = n + 3;
+            if (m >= 0 && n >= 0)
+                Console.WriteLine(calcAnkerman(n, m));
+            else
+            {
+                Console.WriteLine("Для фукнции Анкермана вводимые числа должны быть >= 0!");
+                return;
+            }
 
+            int calcAnkerman(int m, int n)
+            {
+                if (m == 0) return n + 1;
+                if (m != 0 && n == 0) return calcAnkerman(m - 1, 1);
+                if (m > 0 && n > 0) return calcAnkerman(m - 1, calcAnkerman(m, n - 1));
+                return calcAnkerman(n, m);
+            }
         }
     }
 }
