6e7175296a77fdf3b780c1160770686033cbc55e
Maxim Konev <sayberix.ru@yandex.ru>
2022-07-28 21:56:48 +0700

    задача 5.9.2 выполнена

--- Home_work_5.9/Program.cs
+++ Home_work_5.9/Program.cs
@@ -10,7 +10,22 @@ namespace Home_work_5._9
             // M = 1; N = 15-> 120
             // M = 4; N = 8. -> 30
 
+            int m = 4; //Convert.ToInt32(Console.ReadLine());
+            int n = 8; //Convert.ToInt32(Console.ReadLine());
+            int sum = m;
+            Console.WriteLine(Func(m, n));
 
+            string Func(int start, int end)
+            {
+                if (start == end) 
+                    return sum.ToString();
+                else
+                {
+                    sum += start + 1;
+                    return (Func(start + 1, end));
+                }
+                
+            }
         }
     }
 }
