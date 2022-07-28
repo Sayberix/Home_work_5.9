45bca6aceaa092ccdd5705cc4cd30752af5ba6f3
Maxim Konev <sayberix.ru@yandex.ru>
2022-07-28 21:27:22 +0700

    задача 5.9.1 выполнена

--- Home_work_5.9/Program.cs
+++ Home_work_5.9/Program.cs
@@ -10,7 +10,16 @@ namespace Home_work_5._9
             // M = 1; N = 5. -> ""1, 2, 3, 4, 5""
             // M = 4; N = 8. -> ""4, 6, 7, 8""
 
+            int m = 5; //Convert.ToInt32(Console.ReadLine());
+            int n = 25; //Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine(Func(m, n));
+
+            string Func(int start, int end)
+            {
+                if (start == end) return start.ToString();
+                return (start + "," + Func(start + 1, end));
+            }
         }
     }
 }
