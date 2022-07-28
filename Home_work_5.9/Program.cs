8239168d6940f1f60ac99b6fdb650e9d1ed3cda4
Maxim Konev <sayberix.ru@yandex.ru>
2022-07-28 21:58:50 +0700

    условия для задачи 5.9.3

--- Home_work_5.9/Program.cs
+++ Home_work_5.9/Program.cs
@@ -6,26 +6,10 @@ namespace Home_work_5._9
     {
         static void Main(string[] args)
         {
-            // Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
-            // M = 1; N = 15-> 120
-            // M = 4; N = 8. -> 30
+            // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+            // m = 2, n = 3 -> A(m, n) = 29
 
-            int m = 4; //Convert.ToInt32(Console.ReadLine());
-            int n = 8; //Convert.ToInt32(Console.ReadLine());
-            int sum = m;
-            Console.WriteLine(Func(m, n));
 
-            string Func(int start, int end)
-            {
-                if (start == end) 
-                    return sum.ToString();
-                else
-                {
-                    sum += start + 1;
-                    return (Func(start + 1, end));
-                }
-                
-            }
         }
     }
 }
